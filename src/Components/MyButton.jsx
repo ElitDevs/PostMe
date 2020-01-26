13bf2143f49b593d790770f0d70c20e5c6245b33
Baraka Larbi <laridev@gmail.com>
2020-01-26 20:31:52 +0100

    adding first state

--- src/Components/MyButton.jsx
+++ src/Components/MyButton.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
 
 class MyButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0
+    };
+  }
   render() {
-    return <button>{this.props.title}</button>;
+    return (
+      <button>
+        {this.props.title} clicked {this.state.count}
+      </button>
+    );
   }
 }
 
