4acc5f837f0adba94a8b5c289af2504b33bd8a9c
Baraka Larbi <laridev@gmail.com>
2020-01-26 20:24:55 +0100

    :muscul: creating the first component :muscul:

---
+++ src/Components/MyButton.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MyButton extends Component {
+  render() {
+    return <button>Hello i'm an your first Component</button>;
+  }
+}
+
+export default MyButton;
--- src/index.js
+++ src/index.js
@@ -2,4 +2,5 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 // import App from './App';
-ReactDOM.render(<h1>Hello World!!</h1>, document.getElementById("root"));
+import MyButton from "./Components/MyButton";
+ReactDOM.render(<MyButton />, document.getElementById("root"));
