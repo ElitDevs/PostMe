e032158d09b6d2eacb6c53c2b05b8f8a15c5aa9e
AM-77 <amine.griche77@protonmail.ch>
2020-07-02 08:42:34 +0200

    add PostHead

---
+++ src/Containers/PostForm/PostForm.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+import PostHead from '../../Components/PostHead/PostHead'
+
+import './PostForm.css'
+
+export default class PostForm extends Component {
+  render() {
+    return (
+      <div className="postFormContainer">
+        <PostHead />
+      </div>
+    )
+  }
+}
